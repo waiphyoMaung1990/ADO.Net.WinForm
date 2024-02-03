18fe2b6afa61500a9942cc73b1bf71d2f19ca449
ko phyo <taryarphyo1990@gmail.com>
2024-02-03 11:06:57 +0630

    [Complete] WinForm CRUD Dapper

---
+++ DapperWinForm/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace DapperWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            IsActive = new DataGridViewTextBoxColumn();
+            Gender = new DataGridViewTextBoxColumn();
+            LName = new DataGridViewTextBoxColumn();
+            FName = new DataGridViewTextBoxColumn();
+            Id = new DataGridViewTextBoxColumn();
+            dgvEntities = new DataGridView();
+            City = new DataGridViewTextBoxColumn();
+            btnSlect = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnAdd = new Button();
+            txtIsActive = new TextBox();
+            label5 = new Label();
+            txtCity = new TextBox();
+            label4 = new Label();
+            txtGender = new TextBox();
+            label3 = new Label();
+            txtLastName = new TextBox();
+            label2 = new Label();
+            txtFirstName = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvEntities).BeginInit();
+            SuspendLayout();
+            // 
+            // IsActive
+            // 
+            IsActive.HeaderText = "IsActive";
+            IsActive.Name = "IsActive";
+            // 
+            // Gender
+            // 
+            Gender.HeaderText = "Gender";
+            Gender.Name = "Gender";
+            // 
+            // LName
+            // 
+            LName.HeaderText = "LastName";
+            LName.Name = "LName";
+            // 
+            // FName
+            // 
+            FName.HeaderText = "FirstName";
+            FName.Name = "FName";
+            // 
+            // Id
+            // 
+            Id.HeaderText = "Id";
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            // 
+            // dgvEntities
+            // 
+            dgvEntities.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            dgvEntities.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEntities.Columns.AddRange(new DataGridViewColumn[] { Id, FName, LName, Gender, City, IsActive });
+            dgvEntities.Location = new Point(64, 183);
+            dgvEntities.Name = "dgvEntities";
+            dgvEntities.RowTemplate.Height = 25;
+            dgvEntities.Size = new Size(644, 150);
+            dgvEntities.TabIndex = 29;
+            // 
+            // City
+            // 
+            City.HeaderText = "City";
+            City.Name = "City";
+            // 
+            // btnSlect
+            // 
+            btnSlect.Location = new Point(696, 114);
+            btnSlect.Name = "btnSlect";
+            btnSlect.Size = new Size(75, 23);
+            btnSlect.TabIndex = 28;
+            btnSlect.Text = "Select";
+            btnSlect.UseVisualStyleBackColor = true;
+            btnSlect.Click += btnSlect_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(611, 114);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(75, 23);
+            btnUpdate.TabIndex = 27;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(696, 68);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 26;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(611, 68);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 25;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // txtIsActive
+            // 
+            txtIsActive.Location = new Point(486, 65);
+            txtIsActive.Name = "txtIsActive";
+            txtIsActive.Size = new Size(100, 23);
+            txtIsActive.TabIndex = 24;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(408, 68);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 15);
+            label5.TabIndex = 23;
+            label5.Text = "IsActive";
+            // 
+            // txtCity
+            // 
+            txtCity.Location = new Point(288, 111);
+            txtCity.Name = "txtCity";
+            txtCity.Size = new Size(100, 23);
+            txtCity.TabIndex = 22;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(210, 114);
+            label4.Name = "label4";
+            label4.Size = new Size(28, 15);
+            label4.TabIndex = 21;
+            label4.Text = "City";
+            // 
+            // txtGender
+            // 
+            txtGender.Location = new Point(288, 65);
+            txtGender.Name = "txtGender";
+            txtGender.Size = new Size(100, 23);
+            txtGender.TabIndex = 20;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(210, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 15);
+            label3.TabIndex = 19;
+            label3.Text = "Gender";
+            // 
+            // txtLastName
+            // 
+            txtLastName.Location = new Point(100, 111);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(100, 23);
+            txtLastName.TabIndex = 18;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 17;
+            label2.Text = "LastName";
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Location = new Point(100, 62);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(100, 23);
+            txtFirstName.TabIndex = 16;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 15);
+            label1.TabIndex = 15;
+            label1.Text = "FirstName";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvEntities);
+            Controls.Add(btnSlect);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnDelete);
+            Controls.Add(btnAdd);
+            Controls.Add(txtIsActive);
+            Controls.Add(label5);
+            Controls.Add(txtCity);
+            Controls.Add(label4);
+            Controls.Add(txtGender);
+            Controls.Add(label3);
+            Controls.Add(txtLastName);
+            Controls.Add(label2);
+            Controls.Add(txtFirstName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvEntities).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridViewTextBoxColumn IsActive;
+        private DataGridViewTextBoxColumn Gender;
+        private DataGridViewTextBoxColumn LName;
+        private DataGridViewTextBoxColumn FName;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridView dgvEntities;
+        private DataGridViewTextBoxColumn City;
+        private Button btnSlect;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnAdd;
+        private TextBox txtIsActive;
+        private Label label5;
+        private TextBox txtCity;
+        private Label label4;
+        private TextBox txtGender;
+        private Label label3;
+        private TextBox txtLastName;
+        private Label label2;
+        private TextBox txtFirstName;
+        private Label label1;
+    }
+}
---
+++ DapperWinForm/Form1.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+using Dapper;
+using DapperWinForm.Models;
+
+namespace DapperWinForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly IDbConnection dbConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbConnection = InitializeDbConnection();
+            InitializeDataGridView();
+        }
+
+        private IDbConnection InitializeDbConnection()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = ".",
+                InitialCatalog = "Ado,netDb",
+                UserID = "sa",
+                Password = "091537"
+            };
+
+            return new SqlConnection(connectionStringBuilder.ConnectionString);
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Set up DataGridView properties
+            dgvEntities.AutoGenerateColumns = true;
+            dgvEntities.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnSlect_Click(object sender, EventArgs e)
+        {
+            dbConnection.Open();
+            string query = @"
+                SELECT [Id]
+                      ,[FirstName]
+                      ,[LastName]
+                      ,[Gender]
+                      ,[City]
+                      ,[IsActive]
+                  FROM [dbo].[Employees]";
+
+            List<Entities> entitiesList = dbConnection.Query<Entities>(query).ToList();
+
+            dgvEntities.Columns.Clear();
+            dgvEntities.DataSource = entitiesList;
+
+            dbConnection.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Check if any cell is selected
+            if (dgvEntities.SelectedCells.Count > 0)
+            {
+                // Get the selected cell's row index
+                int selectedRowIndex = dgvEntities.SelectedCells[0].RowIndex;
+
+                // Get the 'Id' value from the 'Id' column in the selected row
+                int selectedId = Convert.ToInt32(dgvEntities.Rows[selectedRowIndex].Cells["Id"].Value);
+
+                // Get the user-updated values from the selected cell
+                string updatedFirstName = dgvEntities.Rows[selectedRowIndex].Cells["FirstName"].EditedFormattedValue?.ToString() ?? "";
+                string updatedLastName = dgvEntities.Rows[selectedRowIndex].Cells["LastName"].EditedFormattedValue?.ToString() ?? "";
+                string updatedGender = dgvEntities.Rows[selectedRowIndex].Cells["Gender"].EditedFormattedValue?.ToString() ?? "";
+                string updatedCity = dgvEntities.Rows[selectedRowIndex].Cells["City"].EditedFormattedValue?.ToString() ?? "";
+                string updatedIsActive = dgvEntities.Rows[selectedRowIndex].Cells["IsActive"].EditedFormattedValue?.ToString() ?? "";
+
+                // Update the database with the modified data using Dapper
+                dbConnection.Open();
+                string updateQuery = @"
+                    UPDATE [dbo].[Employees]
+                    SET [FirstName] = @FirstName,
+                        [LastName] = @LastName,
+                        [Gender] = @Gender,
+                        [City] = @City,
+                        [IsActive] = @IsActive
+                    WHERE [Id] = @Id";
+
+                // Use Dapper to execute the query with parameters
+                dbConnection.Execute(updateQuery, new
+                {
+                    FirstName = updatedFirstName,
+                    LastName = updatedLastName,
+                    Gender = updatedGender,
+                    City = updatedCity,
+                    IsActive = updatedIsActive,
+                    Id = selectedId
+                });
+
+                dbConnection.Close();
+                MessageBox.Show("Update successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a cell before updating.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            // Refresh the DataGridView after deletion
+            btnSlect_Click(sender, e);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string query = @"
+            INSERT INTO [dbo].[Employees]
+            ([FirstName], [LastName], [Gender], [City], [IsActive])
+            VALUES
+            (@FirstName, @LastName, @Gender, @City, @IsActive)";
+            Entities blog = new Entities()
+            {
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                City = txtCity.Text,
+                Gender = txtGender.Text,
+                IsActive = txtIsActive.Text,
+            };
+            //      dbConnection= new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+
+
+            int result = dbConnection.Execute(query, blog);
+            string message = result > 0 ? "Saving Successful." : "Saving Failed";
+            MessageBox.Show("Insert successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            // Refresh the DataGridView after deletion
+            btnSlect_Click(sender, e);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Check if any cell is selected
+            if (dgvEntities.SelectedCells.Count > 0)
+            {
+                // Get the selected cell's row index
+                int selectedRowIndex = dgvEntities.SelectedCells[0].RowIndex;
+
+                // Get the 'Id' value from the 'Id' column in the selected row
+                int selectedId = Convert.ToInt32(dgvEntities.Rows[selectedRowIndex].Cells["Id"].Value);
+
+                // Delete the record from the database using Dapper
+                dbConnection.Open();
+                string deleteQuery = "DELETE FROM [dbo].[Employees] WHERE [Id] = @Id";
+
+                // Use Dapper to execute the query with parameters
+                dbConnection.Execute(deleteQuery, new { Id = selectedId });
+
+                dbConnection.Close();
+                MessageBox.Show("Delete successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Refresh the DataGridView after deletion
+                btnSlect_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Please select a cell before deleting.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+}
